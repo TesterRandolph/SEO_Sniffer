73ec08b0d6343f9a87230b1ecc83528edabd9ccf
TesterRandolph <yichiunchang@gmail.com>
2018-12-22 16:02:59 +0800

    Ready to release v1.0.3
    Fixed the coding style by eslint standard.

--- test.js
+++ test.js
@@ -1,47 +1,46 @@
 'use struct'
 
-const fs = require('fs');
-const {PredefineRules, SnifferManager} = require('./lib/SnifferManager');
+const { PredefineRules, SnifferManager } = require('./lib/SnifferManager')
 
 const ioConfig = {
-    // input: './samples/small.htm',
-    input: './samples/shopback_index.htm',
-    // input: fs.createReadStream('./samples/shopback_index.htm'),
+  // input: './samples/small.htm',
+  input: './samples/shopback_index.htm',
+  // input: fs.createReadStream('./samples/shopback_index.htm'),
 
-    output: './logs/test_20181222_9.log'
-    // output: fs.createWriteStream('./logs/test_1.log')
-    // output: 'console'
-};
+  output: './logs/test_20181222_9.log'
+  // output: fs.createWriteStream('./logs/test_1.log')
+  // output: 'console'
+}
 
-const sniffer = new SnifferManager();
-// const sniffer = new SnifferManager(ioConfig);
+const sniffer = new SnifferManager()
+// const sniffer = new SnifferManager(ioConfig)
 
-sniffer.setInput(ioConfig.input);
-// sniffer.setInput(fs.createReadStream(ioConfig.input));
+sniffer.setInput(ioConfig.input)
+// sniffer.setInput(fs.createReadStream(ioConfig.input))
 
-sniffer.setOutput(ioConfig.output);
-// sniffer.setOutput(fs.createWriteStream(ioConfig.output));
+sniffer.setOutput(ioConfig.output)
+// sniffer.setOutput(fs.createWriteStream(ioConfig.output))
 
 PredefineRules.isHeadLegal.subRules.push({
-    rule: 'isTagNotExist',
-    tag: 'meta',
-    attribute: 'name',
-    value: 'robots'
-});
+  rule: 'isTagNotExist',
+  tag: 'meta',
+  attribute: 'name',
+  value: 'robots'
+})
 
-sniffer.reinitDefaultRules();
+sniffer.reinitDefaultRules()
 
-// sniffer.check();
-sniffer.detect();
+// sniffer.check()
+sniffer.detect()
 /*
 sniffer.detect([{
-    attribute: 'name'
-}]);
+  attribute: 'name'
+}])
 */
 /*
 sniffer.detect([
-    // PredefineRules.isImageWithoutAlt,
-    // PredefineRules.isAWithoutRel,
-    PredefineRules.isHeadLegal
-]);
+  // PredefineRules.isImageWithoutAlt,
+  // PredefineRules.isAWithoutRel,
+  PredefineRules.isHeadLegal
+])
 */
